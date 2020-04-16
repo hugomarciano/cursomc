66da9116a30b4c43326c8c4855e631387f993e71
Hugo Marciano <hmarciano@gmail.com>
2020-04-16 15:16:50 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/br/com/marciano/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package br.com.marciano.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.marciano.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
